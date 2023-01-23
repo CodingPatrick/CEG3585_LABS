07fbc0420b1454161c6ef5c12ac517e98b9a5d2a
Pierre <pierreakladios@gmail.com>
2023-01-23 12:42:59 -0500

    selectors

--- server.py
+++ server.py
@@ -1,4 +1,10 @@
 import socket
+import sys
+import selectors
+import types
+
+#selecteur
+sel = selectors.DefaultSelector()
 
 # le serveur n'as besoind d'adresse car il ne va qu'ecouter
 host, port = ("", 4444)
@@ -8,11 +14,15 @@ serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # pour se connecter avec une adresse (on associe le socket avec l'adresse )
 serverSocket.bind((host, port))
 print("Le serveur est en marche ... ")
+serverSocket.listen()
+print(f"Le serveur ecoute sur {(host, port)}")
+#configurer le socket dans le non-blocking mode
+serverSocket.setblocking(False)
+#Enregistre le socket pour etre monitorer avec selectors
+sel.register(serverSocket,selectors.EVENT_READ, data=None)
 
 # on va faire ecouter le serveur sur le port continuellement
 while True:
-    # en lui passe en parametre le nombre de connexion qui peuvent echouer avant de le refuser (facultatif pour py 3.5)
-    serverSocket.listen(5)
     # on va initialiser les connexion pour pouvoir les accepter
     conn, adress = serverSocket.accept()  # adresse = adresse IP + port
     print("Un client vient de se connecter ...")
@@ -23,6 +33,7 @@ while True:
     # on decode la donner
     message = data.decode("utf8")
     print(message)
+    conn.send()
 
 
 conn.close()
