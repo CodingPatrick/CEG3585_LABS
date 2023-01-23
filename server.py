ae4f31595f6bc5119df16ebac3ac2c9d7060dcdb
Pierre <pierreakladios@gmail.com>
2023-01-23 15:51:26 -0500

    adding functions

--- server.py
+++ server.py
@@ -21,6 +21,37 @@ serverSocket.setblocking(False)
 #Enregistre le socket pour etre monitorer avec selectors
 sel.register(serverSocket,selectors.EVENT_READ, data=None)
 
+
+#accepter la connection
+def accept_wrapper(sock):
+    conn, addr = sock.accept()  # Should be ready to read
+    print(f"Accepted connection from {addr}")
+    #Eviter le Hang State pour reduire l'attente
+    conn.setblocking(False)
+    #utiliser les donner pour name space pour savoir quand est ce que le client est pret a lire et écrire
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+#add comments
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # Should be ready to read
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print(f"Closing connection to {data.addr}")
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"Echoing {data.outb!r} to {data.addr}")
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
+
+#FIXME
 # on va faire ecouter le serveur sur le port continuellement
 while True:
     # on va initialiser les connexion pour pouvoir les accepter
