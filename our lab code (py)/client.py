e782329272d7e0ec0df45566e65929d57ecd70e9
Patrick <patrick.loranger.24@gmail.com>
2023-02-03 02:38:15 -0500

    pushing working code

---
+++ our lab code (py)/client.py
@@ -0,0 +1,31 @@
+import socket
+import threading
+
+host, port = ("127.0.0.1", 4444)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((host, port))
+print(f"\nConnected to Server running on host: {host}, on port: {port}")
+
+print('\n###########################################')
+print('# Commands:                               #')
+print('#                                         #')
+print('# Sending a message: to <id> <message>    #')
+print('# Find a name: find                       #')
+print('# Broadcast: <message>                    #')
+print('###########################################')
+
+def send():
+    while True:
+        message = input()
+        client.sendall(message.encode())
+
+def get():
+    while True:
+        data = client.recv(1024).decode()
+        if not data: break
+        print(data)
+
+sending = threading.Thread(target=send)
+sending.start()
+receiving = threading.Thread(target=get)
+receiving.start()
---
+++ our lab code (py)/server.py
@@ -0,0 +1,48 @@
+import socket
+import threading
+
+host, port = ("127.0.0.1", 4444)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen(5)
+print(f"\nServer is running on host: {host}, on port: {port}")
+clients = []
+ids = {}
+
+def add(conn):
+    while True:
+        data = conn.recv(1024)
+        if data.decode().startswith('find'):
+            find(conn)
+        elif data.decode().startswith('to'):
+            receiver, message = data.decode().split(' ', 2)[1:]
+            to(receiver, message, conn)
+        else:
+            broadcast(data, conn)
+
+def find(conn):
+    connections = ''
+    for i in clients:
+        connections = connections + str(ids[i])
+    conn.sendall(f'The client ids currently connected to the server are: {connections}'.encode())
+
+def to(receiver, message, conn):
+    for i in ids.keys():
+        if ids[i][1] == int(receiver):
+            i.sendall(f'Message from {ids[conn]}: {message}'.encode())
+            return
+
+def broadcast(data, conn):
+    for i in clients:
+        if i != conn:
+            i.sendall(f'Message from {ids[conn]}: {data.decode()}'.encode())
+
+while True:
+    conn, address = server.accept()
+    clients.append(conn)
+    ids[conn] = address
+    print(f'{address} is now connected.')
+    conn.sendall(f'\nWelcome {address}!'.encode())
+    thread = threading.Thread(target=add, args=(conn,))
+    thread.start()
