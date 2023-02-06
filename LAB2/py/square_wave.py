0be8e7ca5dbbcbd102cd66dc599ea20fa46d80b2
Patrick <patrick.loranger.24@gmail.com>
2023-02-06 12:22:55 -0500

    squareWave

--- LAB2/py/square_wave.py
+++ LAB2/py/square_wave.py
@@ -21,4 +21,27 @@ def f(t, h=H):
 
         if coefprint:
             print(f"> a0: {a0}    --    a{n} : {round(an,3)}    --    b{n} : {round(bn,3)}")
-        somme = somme + bn
+        somme = somme + bn*math.sin(n*PI*t)
+
+    if coefprint:
+        coefprint = False
+    return somme
+
+print('> == SquareWave == ')
+print(f'> a0        --        an        --        bn')
+
+t=np.arange(STARTI, ENDI, DT)
+ft=[f(i) for i in t]
+fig=plt.figure()
+fig.set_figwidth(10)
+fig.suptitle('squareWave')
+plt.plot(t, ft)
+plt.axhline(0, color='r')
+plt.axvline(0, color='r')
+plt.legend()
+plt.ylabel('f(t)')
+plt.xlabel('t')
+plt.grid(visible=True, which='major', color='#848484', linestyle='-')
+plt.minorticks_on()
+plt.grid(visible=True, which='minor', color='#999999', linestyle='--', alpha=0.34)
+plt.show()
