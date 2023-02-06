88746bb82ec2fb19be68509b51702a4264738f38
Pierre <pierreakladios@gmail.com>
2023-02-06 14:58:38 -0500

    graph generation

--- LAB2/py/rectangular_pulse_train.py
+++ LAB2/py/rectangular_pulse_train.py
@@ -17,3 +17,23 @@ def f(t=T, h=H):
     a0=0
     somme = a0
     global coefprint
+    #to be implemented
+
+print('> == RectangularPulseTrain == ')
+print(f'> a0        --        an        --        bn')
+
+t=np.arange(STARTI, ENDI, DT)
+ft=[f(i) for i in t]
+fig=plt.figure()
+fig.set_figwidth(10)
+fig.suptitle('RectangularPulseTrain')
+plt.plot(t, ft)
+plt.axhline(0, color='r')
+plt.axvline(0, color='r')
+plt.legend()
+plt.ylabel('f(t)')
+plt.xlabel('t')
+plt.grid(visible=True, which='major', color='#848484', linestyle='-')
+plt.minorticks_on()
+plt.grid(visible=True, which='minor', color='#999999', linestyle='--', alpha=0.34)
+plt.show()
