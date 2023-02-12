671824cb1a3989ff2b389a74a418395abff5a13f
Patrick <patrick.loranger.24@gmail.com>
2023-02-12 16:40:24 -0500

    adding rectangular pulse train

--- LAB2/py/rectangular_pulse_train.py
+++ LAB2/py/rectangular_pulse_train.py
@@ -9,34 +9,34 @@ STARTI=-10
 ENDI=10
 DT=0.01
 H=100
-T=2
+T = 2
 
 coefprint=True
 
 def f(t=T, h=H):
-    a0=0
+    a0=5
     somme = a0
     global coefprint
     for n in range(1,h+1):
-        an=0 # to be implemented
-        bn=0 #to be implemented
+        an=0
+        bn=(20/(n*PI))*(1-math.pow(-1,n))/2
 
         if coefprint:
             print(f"> a0: {a0}    --    a{n} : {round(an,3)}    --    b{n} : {round(bn,3)}")
-        somme = somme #to be implemented
+        somme = somme + bn*math.sin(n*PI*t)
 
     if coefprint:
         coefprint = False
     return somme
 
-print('> == RectangularPulseTrain == ')
+print('> == rectangularPulseTrain == ')
 print(f'> a0        --        an        --        bn')
 
 t=np.arange(STARTI, ENDI, DT)
 ft=[f(i) for i in t]
 fig=plt.figure()
 fig.set_figwidth(10)
-fig.suptitle('RectangularPulseTrain')
+fig.suptitle('reactangularPulseTrain')
 plt.plot(t, ft)
 plt.axhline(0, color='r')
 plt.axvline(0, color='r')
