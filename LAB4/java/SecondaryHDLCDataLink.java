97d3e9888f5dfe7beca6c3e7d33988802bbdbc9a
Patrick <patrick.loranger.24@gmail.com>
2023-03-30 00:31:14 -0400

    few changes

--- LAB4/java/SecondaryHDLCDataLink.java
+++ LAB4/java/SecondaryHDLCDataLink.java
@@ -1,27 +1,26 @@
 import java.io.IOException;
 import java.util.ArrayList;
 
-/*
-	// Data Link Layer Entity for Secondary Station
-	// Uses the HDLC protocol for communication over a multipoint link
-	// Assumptions
-	//    Normal Response Mode operation over multi-point link (simulated using PhysicalLayer class over Sockets)
-	//    Use 3-bit sequence numbers
-	// Not Supported:
-	//    FSC checking
-	//    Bit stuffing (frames are transmitted as strings)
-	//  Frames implemented:
-	//     Command Frames:
-	//        NRM: 
-	//        DISC: 
-	//     Response Frames:
-	//        UA: 
-	//     Command/Response Frames:
-	//        I: maximum length of data field is 64 bytes.
-	//        RR: 
-*/
+// Data Link Layer Entity for Secondary Station
+// Uses the HDLC protocol for communication over a multipoint link
+// Assumptions
+//    Normal Response Mode operation over multi-point link (simulated using PhysicalLayer class over Sockets)
+//    Use 3-bit sequence numbers
+// Not Supported:
+//    FSC checking
+//    Bit stuffing (frames are transmitted as strings)
+//  Frames implemented:
+//     Command Frames:
+//        NRM: 
+//        DISC: 
+//     Response Frames:
+//        UA: 
+//     Command/Response Frames:
+//        I: maximum length of data field is 64 bytes.
+//        RR: 
 
-public class SecondaryHDLCDataLink {
+public class SecondaryHDLCDataLink 
+{
 	// Private instance variables
 	private PhysicalLayer physicalLayer; // for sending/receiving frames
 	private int stationAdr; // Station address - not used for the primary station
@@ -29,95 +28,107 @@ public class SecondaryHDLCDataLink {
 	// For the secondary station, used values at index 0
 	private int vs;
 	private int vr;
-	private int rhsWindow; // right hand side of window.
+	private int rhsWindow; // right hand side of window.  
 	private int windowSize; // transmit window size. reception window size is 1.
 	private ArrayList<String> frameBuffer;
-
+	
 	// Constructor
-	public SecondaryHDLCDataLink(int adr) {
-		physicalLayer = new PhysicalLayer();
+	public SecondaryHDLCDataLink(int adr)
+	{
+		physicalLayer = new PhysicalLayer();	
 		stationAdr = adr;
-		vs = 0;
-		vr = 0;
-		windowSize = 4; //
-		frameBuffer = new ArrayList<String>();
-		rhsWindow = vs + windowSize; // seq # < rhsWindow
+	    vs = 0;
+	    vr = 0;
+	    windowSize = 4;  //
+	    frameBuffer = new ArrayList<String>();
+	    rhsWindow = vs+windowSize; // seq # < rhsWindow
 	}
-
-	public void close() throws IOException {
+	
+	public void close() throws IOException
+	{
 		physicalLayer.close();
 	}
-
+	
 	/*----------------------------------------------------------
 	 *  Connection Service
 	 *-----------------------------------------------------------*/
-	// This is a confirmed service, i.e. the return value reflects results from the
-	// confirmation
+	// This is a confirmed service, i.e. the return value reflects results from the confirmation
 
-	public Result dlConnectIndication() { // Receive NRM command frame
+	public Result dlConnectIndication()
+	{  // Receive NRM command frame
 		Result.ResultCode cd = Result.ResultCode.SrvSucessful;
 		int adr = 0;
 		String retStr = null;
 		// Wait for UA response frame
-		String frame = getFrame(true); // true - wait for frame
-		adr = BitString.bitStringToInt(frame.substring(HdlcDefs.ADR_START, HdlcDefs.ADR_END));
+		String frame = getFrame(true);  // true - wait for frame
+		adr = BitString.bitStringToInt(frame.substring(HdlcDefs.ADR_START,HdlcDefs.ADR_END));
 		// Check if frame is U-frame
 		String type = frame.substring(HdlcDefs.TYPE_START, HdlcDefs.TYPE_END);
-		if (type.equals(HdlcDefs.U_FRAME) == false) {
+		if(type.equals(HdlcDefs.U_FRAME) == false)
+		{
 			cd = Result.ResultCode.UnexpectedFrameReceived;
 			retStr = type;
-		} else {
+		}	
+		else
+		{
 			String uframe = frame.substring(HdlcDefs.M1_START, HdlcDefs.M1_END) +
-					frame.substring(HdlcDefs.M2_START, HdlcDefs.M2_END);
-			if (uframe.equals(HdlcDefs.SNRM) == false) {
+			                frame.substring(HdlcDefs.M2_START, HdlcDefs.M2_END);
+			if(uframe.equals(HdlcDefs.SNRM)==false)
+			{
 				cd = Result.ResultCode.UnexpectedUFrameReceived;
 				retStr = uframe;
-			} else
-				System.out.println("Data Link Layer: received SNRM frame >" + BitString.displayFrame(frame) + "<");
+			}
+			else System.out.println("Data Link Layer: received SNRM frame >"+BitString.displayFrame(frame)+"<");
 		}
-		return (new Result(cd, adr, retStr));
+		return(new Result(cd, adr, retStr));		
 	}
 
-	public Result dlConnectResponse() {
+	public Result dlConnectResponse()
+	{
 		Result.ResultCode cd = Result.ResultCode.SrvSucessful;
 		// Check if room for additional connection
-		String frame = HdlcDefs.FLAG + BitString.intToBitString(stationAdr, HdlcDefs.ADR_SIZE_BITS) +
-				HdlcDefs.U_FRAME +
-				HdlcDefs.UA_M1 + HdlcDefs.P1 + HdlcDefs.UA_M2 +
-				HdlcDefs.FLAG;
-		System.out.println("Data Link Layer: prepared UA frame >" + BitString.displayFrame(frame) + "<");
+		String frame = HdlcDefs.FLAG+BitString.intToBitString(stationAdr,HdlcDefs.ADR_SIZE_BITS)+
+		               HdlcDefs.U_FRAME+
+		               HdlcDefs.UA_M1+HdlcDefs.P1+HdlcDefs.UA_M2+
+		               HdlcDefs.FLAG;
+		System.out.println("Data Link Layer: prepared UA frame >"+BitString.displayFrame(frame)+"<");
 		physicalLayer.transmit(frame);
-		vs = 0;
-		vr = 0;
-		return (new Result(cd, stationAdr, null));
+		vs=0;
+		vr=0;
+		return(new Result(cd, stationAdr, null));				
 	}
-
+	
 	/*----------------------------------------------------------
 	 *  Disconnect service - non-confirmed service
-	 *-----------------------------------------------------------*/
-
-	public Result dlDisconnectIndication() { // Disconnection to secondary.
+	 *-----------------------------------------------------------*/	
+	
+	public Result dlDisconnectIndication()
+	{   // Disconnection to secondary.
 		Result.ResultCode cd = Result.ResultCode.SrvSucessful;
 		int adr = 0;
 		String retStr = null;
 		// Wait for DISC frame
-		String frame = getFrame(true); // true - wait for frame
-		adr = BitString.bitStringToInt(frame.substring(HdlcDefs.ADR_START, HdlcDefs.ADR_END));
+		String frame = getFrame(true);  // true - wait for frame
+		adr = BitString.bitStringToInt(frame.substring(HdlcDefs.ADR_START,HdlcDefs.ADR_END));
 		// Check if frame is U-frame
 		String type = frame.substring(HdlcDefs.TYPE_START, HdlcDefs.TYPE_END);
-		if (type.equals(HdlcDefs.U_FRAME) == false) {
+		if(type.equals(HdlcDefs.U_FRAME) == false)
+		{
 			cd = Result.ResultCode.UnexpectedFrameReceived;
 			retStr = type;
-		} else {
+		}	
+		else
+		{
 			String uframe = frame.substring(HdlcDefs.M1_START, HdlcDefs.M1_END) +
-					frame.substring(HdlcDefs.M2_START, HdlcDefs.M2_END);
-			if (uframe.equals(HdlcDefs.DISC) == false) {
+			                frame.substring(HdlcDefs.M2_START, HdlcDefs.M2_END);
+			if(uframe.equals(HdlcDefs.DISC)==false)
+			{
 				cd = Result.ResultCode.UnexpectedUFrameReceived;
 				retStr = uframe;
-			} else
-				System.out.println("Data Link Layer: received DISC frame >" + BitString.displayFrame(frame) + "<");
+			}
+			else System.out.println("Data Link Layer: received DISC frame >"+BitString.displayFrame(frame)+"<");
 		}
-		return (new Result(cd, adr, retStr));
+		return(new Result(cd, adr, retStr));		
 	}
 
 	/*----------------------------------------------------------
@@ -175,100 +186,6 @@ public class SecondaryHDLCDataLink {
 		return (new Result(cd, 0, null));
 	}
 
-	/*------------------------------------------------------------------------
-	 * Helper Methods
-	 *------------------------------------------------------------------------*/
-
-	/*
-	 * // Determines the number of frames acknowledged from the
-	 * // acknowledge number nr.
-	 * // Parameters
-	 * // nr - received ack number - indicates next expected
-	 * // sequence number (nr can equal lhs - window is closed)
-	 * // rhs - right hand side of window - seq number to the
-	 * // right of the last valid number that can be used
-	 * // sz - size of the window
-	 */
-
-	private int checkNr(int nr, int rhs, int sz) {
-		/* Completer cette methode */
-		int lhs;
-
-		if ((rhs - sz) >= 0) {
-			lhs = rhs - sz; // left hand side = right hand side - size of the window
-			return ((nr <= rhs) && (nr >= lhs)) ? nr - lhs : 0;
-		}
-		lhs = sz + rhs; // left hand side = right hand side + size of the window
-		return ((nr <= rhs) || (nr >= lhs)) ? ((sz + nr) % HdlcDefs.SNUM_SIZE_COUNT) - rhs : 0;
-	}
-
-	/*
-	 * // Helper method to get an RR-frame
-	 * // If wait is true then wait until a frame
-	 * // arrives (call getframe(true).
-	 * // If false, return null if no frame
-	 * // is available from the physical layer (call getframe(false)
-	 * // or frame received is not an RR frame.
-	 */
-	private String getRRFrame(boolean wait) {
-
-		// CHANGE ################################################################
-
-		/* Completer cette methode */
-		String frame;
-		frame = getFrame(wait);
-		while (frame == null && wait) {
-			frame = getFrame(wait);
-			if (!(frame != null && (frame.substring(HdlcDefs.S_START, HdlcDefs.S_END).equals(HdlcDefs.RR_SS))
-					&& (frame.substring(HdlcDefs.TYPE_START, HdlcDefs.TYPE_END).equals(HdlcDefs.S_FRAME))))
-				return frame = null;
-		}
-		return (frame);
-	}
-
-	/*
-	 * // For displaying the status of variables used
-	 * // in exchanging data between stations.
-	 */
-	private void displayDataXchngState(String msg) {
-		int lhs; // left hand side of the window
-		// compute lhs
-		if ((rhsWindow - windowSize) >= 0)
-			lhs = rhsWindow - windowSize;
-		else
-			lhs = rhsWindow - windowSize + HdlcDefs.SNUM_SIZE_COUNT;
-
-		System.out.println("Data Link Layer: Station " + stationAdr + ": " + msg);
-		System.out.println("    v(s) = " + vs + ", v(r) = " + vr +
-				", Window: lhs=" + lhs + " rhs=" + rhsWindow +
-				", Number frames buffered = " + frameBuffer.size());
-	}
-
-	/*
-	 * // Waits for reception of frame
-	 * // If wait is true, then wait for a frame to arrive,
-	 * // otherwise just poll physical layer for a frame.
-	 * // Returns null if no frame is received.
-	 */
-	private String getFrame(boolean wait) {
-		// Only frames with this stations address is processed - others are ignored
-		String frame = null;
-		do {
-			if (wait)
-				frame = physicalLayer.receive(); // block on receive.
-			else
-				frame = physicalLayer.pollReceive(); // get frame from physical layer
-			if (frame != null) {
-				int adr = BitString.bitStringToInt(frame.substring(HdlcDefs.ADR_START, HdlcDefs.ADR_END));
-				if (adr != stationAdr)
-					frame = null; // ignore strings for other destinations
-			}
-		} while (frame == null && wait);
-		// if(frame != null) System.out.println("Data Link Layer: Received frame
-		// >"+BitString.displayFrame(frame)+"<");
-		return (frame);
-	}
-
 	private void sendIFrame(String info, int frameNumber, boolean isFinal) {
 		String finalBit = isFinal ? "1" : "0";
 
@@ -282,5 +199,86 @@ public class SecondaryHDLCDataLink {
 
 		// Envoy le frame
 		physicalLayer.transmit(frame);
+	}
+
+	/*------------------------------------------------------------------------
+	 * Helper Methods
+	 *------------------------------------------------------------------------*/
+
+	// Determines the number of frames acknowledged from the
+	// acknowledge number nr.
+	// Parameters
+	// nr - received ack number - indicates next expected
+	//      sequence number (nr can equal lhs - window is closed)
+	// rhs - right hand side of window - seq number to the
+	//       right of the last valid number that can be used
+	// sz - size of the window
+	private int checkNr(int nr, int rhs, int sz) {
+		/* Completer cette methode */
+		int lhs;
+		if ((rhs - sz) >= 0) {
+			lhs = rhs - sz; // left hand side = right hand side - size of the window
+			return ((nr <= rhs) && (nr >= lhs)) ? nr - lhs : 0;
+		}
+		lhs = sz + rhs; // left hand side = right hand side + size of the window
+		return ((nr <= rhs) || (nr >= lhs)) ? ((sz + nr) % HdlcDefs.SNUM_SIZE_COUNT) - rhs : 0;
+	}
+
+	// Helper method to get an RR-frame
+	// If wait is true then wait until a frame
+	// arrives (call getframe(true).
+	// If false, return null if no frame
+	// is available from the physical layer (call getframe(false)
+	// or frame received is not an RR frame.
+	private String getRRFrame(boolean wait) {
+		/* Completer cette methode */
+		String frame;
+		frame = getFrame(wait);
+		while (frame == null && wait) {
+			frame = getFrame(wait);
+			boolean condition1 = frame.substring(HdlcDefs.S_START, HdlcDefs.S_END).equals(HdlcDefs.RR_SS);
+			boolean condition2 = frame.substring(HdlcDefs.TYPE_START, HdlcDefs.TYPE_END).equals(HdlcDefs.S_FRAME);
+			if (!(frame != null && condition1 && condition2))
+				frame = null;
+				return (frame);
+		}
+		return (frame);
+	}
+
+	// For displaying the status of variables used
+	// in exchanging data between stations.
+	private void displayDataXchngState(String msg)
+	{
+		int lhs; // left hand side of the window
+		//compute lhs
+		if( (rhsWindow-windowSize) >= 0) lhs = rhsWindow - windowSize;
+		else lhs = rhsWindow - windowSize + HdlcDefs.SNUM_SIZE_COUNT;
+
+		System.out.println("Data Link Layer: Station "+stationAdr+": "+msg);
+		System.out.println("    v(s) = "+vs+", v(r) = "+vr+
+				           ", Window: lhs="+lhs+" rhs="+rhsWindow+
+				           ", Number frames buffered = "+frameBuffer.size());
+	}
+
+	// Waits for reception of frame
+	// If wait is true, then wait for a frame to arrive,
+	// otherwise just poll physical layer for a frame.
+	// Returns null if no frame is received.
+	private String getFrame(boolean wait)
+	{
+		// Only frames with this stations address is processed - others are ignored
+		String frame = null;
+		do
+		{
+			if(wait) frame = physicalLayer.receive(); // block on receive.
+			else frame = physicalLayer.pollReceive();  // get frame from physical layer
+			if(frame != null)
+			{
+				int adr = BitString.bitStringToInt(frame.substring(HdlcDefs.ADR_START, HdlcDefs.ADR_END));
+				if(adr != stationAdr) frame = null;  // ignore strings for other destinations			
+			}
+		} while(frame == null && wait);
+		//if(frame != null) System.out.println("Data Link Layer: Received frame >"+BitString.displayFrame(frame)+"<");
+		return(frame);
 	}
 }
